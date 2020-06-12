3b48f3fb9c3563c1a87e592d574fe24ece92c68e
Lee Hoyong <igotoweb@gmail.com>
2020-06-12 22:12:32 +0900

    update the schema for synchronization with orm version

--- server/schema.sql
+++ server/schema.sql
@@ -6,12 +6,16 @@ CREATE TABLE messages (
   roomname varchar(255),
   text text(1024),
   userid varchar(255) NOT NULL,
+  createdAt datetime DEFAULT CURRENT_TIMESTAMP,
+  updatedAt datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 );
 /* Create other tables and define schemas for them here! */
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT,
   username varchar(255),
+  createdAt datetime DEFAULT CURRENT_TIMESTAMP,
+  updatedAt datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 );
 /*  Execute this file from the command line by typing:
